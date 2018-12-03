3d432da50f7b0a405055292e3b8ecbbf0f7084fb
trie94 <trie60@gmail.com>
2018-12-02 23:53:40 -0500

    inheritance

--- sketches/BaseSketch.js
+++ sketches/BaseSketch.js
@@ -1,3 +1,30 @@
-export default function BaseSketch() {
+export default function BaseSketch(scene) {
+    bindEventListeners();
+    start();
+    update();
 
+    function bindEventListeners() {
+        window.onresize = resizeCanvas;
+        window.onclick = mouseClick;
+        resizeCanvas();
+    }
+
+    function resizeCanvas() {
+        canvas.style.width = window.innerWidth;
+        canvas.style.height = window.innerHeight;
+        scene.onWindowResize();
+    }
+
+    function mouseClick() {
+        scene.onMouseClick();
+    }
+
+    function start() {
+        scene.start();
+    }
+
+    function update() {
+        requestAnimationFrame(update);
+        scene.update();
+    }
 }
--- sketches/sketch4/index.js
+++ sketches/sketch4/index.js
@@ -1,40 +1,14 @@
 import * as THREE from 'three';
 const OrbitControls = require('three-orbit-controls')(THREE);
 import Scene from './Scene';
+import BaseSketch from '../BaseSketch';
 
 export default function Sketch4() {
     const canvas = document.createElement('canvas');
     canvas.id = 'canvas';
     const root = document.getElementById('root');
     root.appendChild(canvas);
+    
     const scene = new Scene(canvas);
-
-    bindEventListeners();
-    start();
-    update();
-
-    function bindEventListeners() {
-        window.onresize = resizeCanvas;
-        window.onclick = mouseClick;
-        resizeCanvas();
-    }
-
-    function resizeCanvas() {
-        canvas.style.width = window.innerWidth;
-        canvas.style.height = window.innerHeight;
-        scene.onWindowResize();
-    }
-
-    function mouseClick() {
-        scene.onMouseClick();
-    }
-
-    function start() {
-        scene.start();
-    }
-
-    function update() {
-        requestAnimationFrame(update);
-        scene.update();
-    }
+    Sketch4.prototype = new BaseSketch(scene);
 }
